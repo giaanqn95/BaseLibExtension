19c162b1b17ac701d9f02d0a9160b05e5027d118
Trần Đỗ Gia Ân <>
2020-11-24 17:35:21 +0700

    Update example flow

--- app/src/main/java/vn/com/baselibextension/MainActivity.kt
+++ app/src/main/java/vn/com/baselibextension/MainActivity.kt
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 import vn.com.baselibextension.extensions.launchWhenStartedUntilStopped
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private var listA = arrayListOf<String>()
+    private var listB = arrayListOf<Int>()
+
+    private val flowS: Flow<Int> = flow {
+        for (i in 0..3) {
+            emit(i)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +28,23 @@ class MainActivity : AppCompatActivity() {
         viewModel.stateFlow.onEach {
 
         }.launchWhenStartedUntilStopped(this)
+        runBlocking {
+            viewModel.stateFlow.onEach {
+                print(it)
+            }.collect {
+
+            }
+            flowS.filter { it % 2 == 0 }.collect { }
+
+            val result = sequence {
+                val first = listA.iterator()
+                val second = listB.iterator()
+                while (first.hasNext() && second.hasNext()) {
+                    yield(NewObject(first.next(), second.next()))
+                }
+            }.toList()
+        }
     }
 }
+
+class NewObject(val a: String, val b: Int)
